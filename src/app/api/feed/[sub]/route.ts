7fb30ffc33f16fb1e8f35be9f976e3299d0e24c8
sp <sp@hyrme.com>
2023-08-25 09:46:25 +0800

    cleanup: remove nextjs-pwa generated files bugfix: added test routes for subreddits

--- src/app/api/feed/[sub]/route.ts
+++ src/app/api/feed/[sub]/route.ts
@@ -2,24 +2,24 @@ import { NextResponse } from 'next/server';
 import { promises as fs } from 'fs';
 
 //for testing retrieve saved feed json.
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const page = searchParams.get('page')
-  //Find the absolute path of the json directory
-  const jsonDirectory = process.cwd()
-  console.log('getting next page', page)
-  //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + '/test.json', 'utf8');
-  //Return the content of the data file in json format
-  return NextResponse.json(JSON.parse(fileContents));
-}
-
-export async function GETx(request: Request,  { params }: { params: { sub: string } }) {
-  const { searchParams } = new URL(request.url)
-  const page = searchParams.get('page')
-  //Find the absolute path of the json directory
-  const results = await fetch(`https://old.reddit.com/r/${params.sub}.json?after=${page}`)
-  const json = await results.json()
-  //Return the content of the data file in json format
-  return NextResponse.json(json);
+export async function GET(request: Request, { params }: { params: { sub: string } }) {
+  if (process.env.TEST_MODE) {
+    const { searchParams } = new URL(request.url)
+    const page = searchParams.get('page')
+    //Find the absolute path of the json directory
+    const jsonDirectory = process.cwd()
+    console.log('getting next page', page)
+    //Read the json data file data.json
+    const fileContents = await fs.readFile(jsonDirectory + '/test.json', 'utf8');
+    //Return the content of the data file in json format
+    return NextResponse.json(JSON.parse(fileContents));
+  } else {
+    const { searchParams } = new URL(request.url)
+    const page = searchParams.get('page')
+    //Find the absolute path of the json directory
+    const results = await fetch(`https://old.reddit.com/r/${params.sub}.json?after=${page}`)
+    const json = await results.json()
+    //Return the content of the data file in json format
+    return NextResponse.json(json);
+  }
 }
--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -1,26 +1,24 @@
 import { promises as fs } from 'fs';
 import { NextResponse } from 'next/server';
 
-
-
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const page = searchParams.get('page')
-  //Find the absolute path of the json directory
-  const jsonDirectory = process.cwd()
-  console.log('getting next page', page)
-  //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + '/test.json', 'utf8');
-  //Return the content of the data file in json format
-  return NextResponse.json(JSON.parse(fileContents));
-}
-
-export async function GETx(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const page = searchParams.get('page')
-  //Find the absolute path of the json directory
-  const results = await fetch(`https://old.reddit.com/.json?after=${page}`)
-  const json = await results.json()
-  //Return the content of the data file in json format
-  return NextResponse.json(json);
+  if (process.env.TEST_MODE) {
+    const { searchParams } = new URL(request.url)
+    const page = searchParams.get('page')
+    //Find the absolute path of the json directory
+    const jsonDirectory = process.cwd()
+    console.log('getting next page', page)
+    //Read the json data file data.json
+    const fileContents = await fs.readFile(jsonDirectory + '/test.json', 'utf8');
+    //Return the content of the data file in json format
+    return NextResponse.json(JSON.parse(fileContents));
+  } else {
+    const { searchParams } = new URL(request.url)
+    const page = searchParams.get('page')
+    //Find the absolute path of the json directory
+    const results = await fetch(`https://old.reddit.com/.json?after=${page}`)
+    const json = await results.json()
+    //Return the content of the data file in json format
+    return NextResponse.json(json);
+  }
 }
