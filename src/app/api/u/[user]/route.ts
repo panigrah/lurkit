8f6bcadee1a300fbf8cfc5ae0e2396a4d9e44c68
sp <vinedb@hyrme.com>
2024-01-12 12:54:24 +0800

    updated page id in user

--- src/app/api/u/[user]/route.ts
+++ src/app/api/u/[user]/route.ts
@@ -6,7 +6,7 @@ export async function GET(
   { params }: { params: { user: string } }
 ) {
   const { searchParams } = new URL(request.url);
-  const page = searchParams.get("page");
+  const page = searchParams.get("after");
   const count = searchParams.get("count") || 25;
   //Find the absolute path of the json directory
   const results = await fetch(
